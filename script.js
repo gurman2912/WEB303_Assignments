ae328aafe926ee194b7e783a65e34a0ab00d411c
gurman2912 <w0830963@myscc.ca>
2023-09-17 14:51:02 -0400

    create script file

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	{Your Name Here}
+	Name - Gurmandeep Kaur
+	Student No. - 0830963
+	Date - 2023-09-17
 */
 
+$(document).ready(function() {
+	// Event handler for "change" event on the salary and percent fields
+	$('#yearly-salary, #percent').on('change', function() {
+	  // Get the values from the input fields
+	  var salary = parseFloat($('#yearly-salary').val());
+	  var percent = parseFloat($('#percent').val());
+  
+	  // Calculate the amount and format it with a dollar sign
+	  var amount = (salary * percent / 100).toFixed(2);
+	  $('#amount').text('$' + amount);
+	});
+  });
+  
