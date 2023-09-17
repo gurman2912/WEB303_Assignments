042436bb79eb9029f735f46943553f8a6971ce89
gurman2912 <w0830963@myscc.ca>
2023-09-17 16:44:33 -0400

    update

--- script.js
+++ script.js
@@ -6,15 +6,15 @@
 */
 
 $(document).ready(function() {
-	// Event handler for "change" event on the salary and percent fields
-	$('#yearly-salary, #percent').on('change', function() {
-	  // Get the values from the input fields
-	  var salary = parseFloat($('#yearly-salary').val());
-	  var percent = parseFloat($('#percent').val());
+    // Event handler for "change" event on the salary and percent fields
+    $('#yearly-salary, #percent').on('change', function() {
+      // Get the values from the input fields
+      let salary = parseFloat($('#yearly-salary').val());
+      let percent = parseFloat($('#percent').val());
   
-	  // Calculate the amount and format it with a dollar sign
-	  var amount = (salary * percent / 100).toFixed(2);
-	  $('#amount').text('$' + amount);
-	});
+      // Calculate the amount and format it with a dollar sign
+      let amount = (salary * percent / 100).toFixed(2);
+      $('#amount').text('$' + amount);
+    });
   });
   
